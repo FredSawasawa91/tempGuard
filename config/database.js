7cfdbed08f6e9af673b2625d2daac03c99e406e2
FRED <fredsawa91@gmail.com>
2026-06-11 23:31:50 +0200

    third commit

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+const path = require("path");
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB_STORAGE || path.join(__dirname, '../database.sqlite'),
-  logging: false, // Set to console.log to see SQL queries
+  dialect: "sqlite",
+  storage: path.join(__dirname, "../database.sqlite"), // DB file
+
+  logging: false,
+
   pool: {
     max: 5,
     min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
